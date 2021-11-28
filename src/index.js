10d32843fd9fe0cb527ea253a4e3cefa6f682d4d
Darin376 <Sleeper2012@yndex.ru>
2021-11-28 18:46:06 +0300

    twocommit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
 <h1>Hello World</h1>,
   document.getElementById('root')
 );
-
-
-reportWebVitals();
